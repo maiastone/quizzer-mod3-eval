f2f53d2932e472fbcbf587e35f01eb05c408fb77
Maia Stone <maiadov@gmail.com>
2017-01-07 17:39:05 -0700

    WIP setup axios

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quizzes: null,
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/quizzes')
+    .then((response) => {
+      this.setState({
+        quizzes: response.data.quizzes
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Quizzer App</h2>
+        </div>
+        {this.state.quizzes}
+      </div>
+    );
+  }
+}
+
+export default App;
